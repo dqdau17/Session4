f83a4edec7905147e2add330ea5dc6cbf3689a88
phantasmlance <qdau79@gmail.com>
2021-03-12 17:07:01 +0700

    Initial commit

--- Session4/Connect.cs
+++ Session4/Connect.cs
@@ -2,6 +2,6 @@
 {
     public static class Connect
     {
-        public static Session4Entities db = new Session4Entities();
+        public static Session4Entities1 db = new Session4Entities1();
     }
 }
